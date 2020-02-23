5b96eb0bff6c02feb803f4a02aaed4d2156e99ea
cawilson1 <>
2020-02-23 10:40:57 -0500

    finish basic vdom

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import createElement from "./vdom/createElement";
 import render from "./vdom/render";
 import mount from "./vdom/mount";
+import diff from "./vdom/diff";
 
 const createVApp = count =>
   createElement("div", {
@@ -9,29 +10,40 @@ const createVApp = count =>
       dataCount: count
     },
     children: [
+      createElement("input"),
       String(count), //each dom node has two nodes, text (this one) and element (below)
-      createElement("img", {
+      createElement("p", {
         attrs: {
-          id: "none",
-          src: "https://media.giphy.com/media/6bBVmOPLsLZ3G/giphy.gif"
+          id: "wow"
         },
         children: [
-          String(count), //each dom node has two nodes, text (this one) and element (below)
-          createElement("p", {
-            attrs: {
-              id: "notthis"
-              //src: "https://media.giphy.com/media/6bBVmOPLsLZ3G/giphy.gif"
-            }
-          })
+          String("test"), //each dom node has two nodes, text (this one) and element (below)
+          ...Array.from({ length: count }, () =>
+            createElement("img", {
+              attrs: {
+                id: `image${count}`,
+                src: "https://media.giphy.com/media/6bBVmOPLsLZ3G/giphy.gif"
+              }
+            })
+          )
         ]
       })
     ]
   });
 
 let count = 0;
-const vApp = createVApp(count);
+let vApp = createVApp(count);
 const $app = render(vApp);
 
-mount($app, document.getElementById("app"));
+let $rootEl = mount($app, document.getElementById("app"));
 
-console.log($app);
+setInterval(() => {
+  count++;
+  const vNewApp = createVApp(count);
+  //const vNewApp = createVApp(Math.floor(Math.random() * 5));
+  const patch = diff(vApp, vNewApp);
+  $rootEl = patch($rootEl);
+  vApp = vNewApp;
+}, 1000);
+
+console.log("$app", $app);
--- src/vdom/render.js
+++ src/vdom/render.js
@@ -1,23 +1,31 @@
 const renderElem = ({ tagName, attrs, children }) => {
   const $el = document.createElement(tagName);
+  console.log("tagname", tagName);
   //set attributes
   for (const [k, v] of Object.entries(attrs)) {
     //gets array of all entries in the object
+    console.log(k, "and", v);
     $el.setAttribute(k, v);
   }
+  console.log("$el minus child", $el);
   //set children
+  console.log(children.length);
   for (const child of children) {
     const $child = render(child);
     $el.appendChild($child);
   }
 
+  console.log("before return", $el);
   return $el;
 };
 
 const render = vNode => {
+  console.log("firstvnode", vNode);
   if (typeof vNode === "string") {
+    console.log("string", vNode);
     return document.createTextNode(vNode);
   } else {
+    console.log("node", vNode);
     return renderElem(vNode);
   }
 };
